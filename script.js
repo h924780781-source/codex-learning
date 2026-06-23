10738ab6fc56f98d3294d3726cc0f99c84948c95
h924780781-source <h924780781@gmail.com>
2026-06-23 21:31:20 +0900

    新增修改标题按钮

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function changeMessage() {
   const message = document.getElementById("message");
   message.textContent = "我已经开始学习 JavaScript 了！";
 }
+function changeTitle() {
+  const title = document.getElementById("title");
+  title.textContent = "我正在学习前端";
+}
