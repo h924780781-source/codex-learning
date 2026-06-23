8cb1aa4fa12beeb9a7429d6640cd72a9a542e96e
h924780781-source <h924780781@gmail.com>
2026-06-23 21:50:29 +0900

    新增输入框打招呼功能

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function changeTitle() {
   const title = document.getElementById("title");
   title.textContent = "我正在学习前端";
 }
+function sayHello() {
+  const nameInput = document.getElementById("nameInput");
+  const helloResult = document.getElementById("helloResult");
+
+  helloResult.textContent = "你好，" + nameInput.value;
+}
